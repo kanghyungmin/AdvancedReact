1d573624d82b1382ec7a88ed8a0032b01f6e021a
hyungmin <hmkkang0922@daum.net>
2024-02-13 11:30:05 +0900

    add : Composition Pattern

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-import { UserInfo } from "./component/user-info";
+// import { GreenSmallButton, RedButton } from "./component/composition";
+
+import { RedButton, SmallRedButton } from "./component/partial";
 
 function App() {
   return (
     <>
-      <UserInfo userId={"3"}></UserInfo>
+      <RedButton text="I am red!" />
+      <SmallRedButton text="I am small and red" />
     </>
   );
 }
---
+++ src/component/recursive.jsx
@@ -0,0 +1,22 @@
+const isObject = (data) => typeof "object" && data !== null;
+
+export const RecursiveComponent = ({ data }) => {
+  if (!isObject(data)) {
+    return <li>{data}</li>;
+  }
+
+  const pairs = Object.entries(data);
+
+  return (
+    <>
+      {pairs.map(([key, value]) => {
+        <li>
+          {key}:
+          <ul>
+            <RecursiveComponent data={value} />
+          </ul>
+        </li>;
+      })}
+    </>
+  );
+};
